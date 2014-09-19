c9ea88517da913dd842a56a2b8d6ec8541af0136
Roshan Piyush <piyushroshan@gmail.com>
2014-09-19 22:11:19 +0530

    Create tmux-zoom.sh

---
+++ tmux-zoom.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -f
+currentwindowname=`tmux list-window | tr '\t' ' ' | sed -n -e '/(active)/s/^[^:]*: *\([^ ]*\) .*/\1/gp'`;
+currentwindow=`tmux list-window | tr '\t' ' ' | grep -n active | sed 's/:.*//'`;
+currentpane=`tmux list-panes | tr '\t' ' ' | grep -n active | sed 's/:.*//'`;
+panecount=`tmux list-panes | wc -l`;
+inzoom=`tmux list-window | tr '\t' ' ' | grep -n active | grep -n zoom | sed 's/:.*//'`
+if [ $panecount -ne 1 ]; then
+    inzoom="";
+fi
+if [ $inzoom ]; then
+    lastpane=`echo $currentwindowname | rev | cut -f 1 -d '@' | rev`;
+    lastwindow=`echo $currentwindowname | cut -f 2- -d '@' | rev | cut -f 2- -d '@' | rev`;
+    tmux select-window -t $lastwindow;
+    tmux select-pane -t $lastpane;
+    tmux swap-pane -s $currentwindow;
+    
+    tmux kill-window -t $currentwindow;
+#   echo $lastwindow
+#   echo $lastpane
+#   echo $currentwindow
+else
+    newwindowname=zoom@$currentwindow@$currentpane;
+    tmux new-window -d -n $newwindowname;
+    tmux swap-pane -s $newwindowname;
+    tmux select-window -t $newwindowname;
+fi
